ac017e63ecd9a4a0e596a8213ef29e7a0850e439
tu0ng_c0ng <tcvn1985@gmail.com>
2014-03-13 16:31:18 +0700

    Cập nhật tiêu đề, thông tin
    Cập nhật lại tiêu đề, giới thiệu

--- Source/tai-source-list.sh
+++ Source/tai-source-list.sh
@@ -1,4 +1,9 @@
- wget https://github.com/tothanhcong/scripts/blob/master/sources.list
- cp /etc/apt/sources.list /etc/apt/sources.list.bka
- rm -rf /etc/apt/sources.list
- cp sources.list /etc/apt/sources.list
+#Shell thu hien viec tai va thuc thu file source.list, chuyen thanh cac repos o VN
+###
+#tcvn1985@gmail.cm
+##====
+
+wget https://raw.github.com/tothanhcong/scripts/master/Source/sources.list
+cp /etc/apt/sources.list /etc/apt/sources.list.bka
+rm -rf /etc/apt/sources.list
+cp sources.list /etc/apt/sources.list
