de8a25f5fb85ecca5c426586af9156f2dd93487c
tothanhcong <tcvn1985@gmail.com>
2014-03-13 16:24:11 +0700

    Đưa vào thư mục
    Đưa các file vào thư mục

---
+++ Source/tai-source-list.sh
@@ -0,0 +1,4 @@
+ wget https://github.com/tothanhcong/scripts/blob/master/sources.list
+ cp /etc/apt/sources.list /etc/apt/sources.list.bka
+ rm -rf /etc/apt/sources.list
+ cp sources.list /etc/apt/sources.list
